bd68a6555e1da95184d83d6e893635a8ef4a43d1
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 13:57:08 +0300

    refactor: update DUMMY_DATA

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -7,7 +7,52 @@ import { AIMessage } from "../../types/api";
 const DUMMY_DATA: Array<UserMessage | AIMessage> = [
   {
     author: "user",
-    text: "I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take?",
+    text: "I have just started my journey to growing. What should I take?",
+  },
+  {
+    author: "ai",
+    text: "Certainly! Here are a few Grown sets that are suitable for beginners.",
+    products: [
+      {
+        title: { name: "Lavender Feminized Cannabis Seeds", link: "/" },
+        description: "A great rainy day smoke",
+        cost: 9.58,
+        linkDetails: "/",
+        addToCart: "/",
+        review: {
+          grade: 5,
+          amount: 7,
+          link: "/",
+        },
+        img: "../../components/app/productImages/product1.png",
+      },
+      {
+        title: { name: "Lemon Autoflower Cannabis Seeds", link: "/" },
+        description: "Lemon Skunk dragged into the supersonic age",
+        cost: 9.25,
+        linkDetails: "/",
+        addToCart: "/",
+        review: {
+          grade: 5,
+          amount: 17,
+          link: "/",
+        },
+        img: "../../components/app/productImages/product2.png",
+      },
+      {
+        title: { name: "Blueberry Gusto Auto Cannabis Seeds", link: "/" },
+        description: "Fast, exploisive fruit",
+        cost: 9.58,
+        linkDetails: "/",
+        addToCart: "/",
+        review: {
+          grade: 5,
+          amount: 7,
+          link: "/",
+        },
+        img: "../../components/app/productImages/product3.png",
+      },
+    ],
   },
 ];
 
