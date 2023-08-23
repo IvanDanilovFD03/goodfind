7ceed327a982be9a803c4a08bce5132d7f727010
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 13:04:07 +0300

    feat: add MessageList to MessageWidget

--- src/components/MessageWidget/index.tsx
+++ src/components/MessageWidget/index.tsx
@@ -3,21 +3,27 @@ import { useBreakpoints } from "../../hooks/useBreakpoints";
 import { Box } from "../ui/Box";
 import { styles } from "./styles";
 import { Header } from "../Header";
+import { MessageList } from "../MessageList";
+import { UserMessage } from "../../types/api";
+import { AIMessage } from "../../types/api";
 
 export interface MessageWidgetProps {
   widgetTitle: string;
   messageWidgetOpen: boolean;
   setMessageWidgetOpen: (value: React.SetStateAction<boolean>) => void;
+  messages: Array<UserMessage | AIMessage>;
 }
 
 export const MessageWidget: FC<MessageWidgetProps> = ({
   widgetTitle,
   messageWidgetOpen,
   setMessageWidgetOpen,
+  messages,
 }) => {
   return (
     <Box sx={styles.root}>
       <Header title={widgetTitle} setMessageWidgetOpen={setMessageWidgetOpen} />
+      <MessageList messages={messages} />
     </Box>
   );
 };
