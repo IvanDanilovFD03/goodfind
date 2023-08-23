f5b6b4c074f187d6d29ec8c7f0c4c40d59f72c80
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 13:05:05 +0300

    feat: add DUMMY_DATA to MainPage and update MainPageView's props

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
 
 import { MainPageView } from "../MainPageView/MainPageView";
+import { UserMessage } from "../../types/api";
+import { AIMessage } from "../../types/api";
+
+const DUMMY_DATA: Array<UserMessage | AIMessage> = [
+  {
+    author: "user",
+    text: "I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take? I have just started my journey to growing. What should I take?",
+  },
+];
 
 const MainPage: FC = () => {
-  return <MainPageView />;
+  return <MainPageView messages={DUMMY_DATA} />;
 };
 
 export default MainPage;
