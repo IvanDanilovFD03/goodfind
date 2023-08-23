1ab4d3919564c2a489c4d365c1de447aa48f1ca6
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 13:03:48 +0300

    feat: add list of messages to MessageList component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,10 +1,26 @@
 import { FC, useState } from "react";
 import { useBreakpoints } from "../../hooks/useBreakpoints";
 import { Box } from "../ui/Box";
+import { List } from "../ui/List";
+import { ListItem } from "../ui/ListItem";
+
 import { styles } from "./styles";
+import { Message } from "../Message";
+import { UserMessage } from "../../types/api";
+import { AIMessage } from "../../types/api";
 
-export interface MessageListProps {}
+export interface MessageListProps {
+  messages: Array<UserMessage | AIMessage>;
+}
 
-export const MessageList: FC<MessageListProps> = ({}) => {
-  return <Box sx={styles.root}></Box>;
+export const MessageList: FC<MessageListProps> = ({ messages }) => {
+  return (
+    <List sx={styles.root}>
+      {messages.map((message) => (
+        <ListItem sx={styles.listItem}>
+          <Message author={message.author} text={message.text} />
+        </ListItem>
+      ))}
+    </List>
+  );
 };
--- src/components/MessageList/styles.ts
+++ src/components/MessageList/styles.ts
@@ -1,4 +1,13 @@
 import { SxStyles } from "../../types/styles";
 import { basicTheme } from "../../theme";
 
-export const styles: SxStyles = {};
+export const styles: SxStyles = {
+  root: {
+    width: "100%",
+    p: 0,
+  },
+  listItem: {
+    p: 0,
+    width: "100%",
+  },
+};
