0c67fc2f86c6eb215195e6b4f502c92aba646ab1
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 11:30:10 +0300

    feat: create MainPage

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { MainPageView } from "../MainPageView/MainPageView";
+
+const MainPage: FC = () => {
+  return <MainPageView />;
+};
+
+export default MainPage;
