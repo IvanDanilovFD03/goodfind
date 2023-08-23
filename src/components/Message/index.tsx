732fead9dbfda6d5ea27f55f33de95ee587e73a3
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 13:03:06 +0300

    feat: add user variant of Message component

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,10 +1,27 @@
 import { FC, useState } from "react";
 import { useBreakpoints } from "../../hooks/useBreakpoints";
+import { Avatar } from "../ui/Avatar";
 import { Box } from "../ui/Box";
 import { styles } from "./styles";
+import { Typography } from "../ui/Typography";
+import userImage from "../app/userImage/userImage.png";
 
-export interface MessageProps {}
+export interface MessageProps {
+  author: "user" | "ai";
+  text: string;
+}
 
-export const Message: FC<MessageProps> = ({}) => {
-  return <Box sx={styles.root}></Box>;
+export const Message: FC<MessageProps> = ({ author, text }) => {
+  return (
+    <Box sx={styles.root}>
+      {author === "user" && (
+        <Box sx={styles.userMessage}>
+          <Avatar alt="user image" variant="square" src={userImage} sx={styles.authorIcon}/>
+          <Typography variant="textMessage" sx={styles.userText}>
+            {text}
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
 };
--- src/components/Message/styles.ts
+++ src/components/Message/styles.ts
@@ -1,4 +1,20 @@
 import { SxStyles } from "../../types/styles";
 import { basicTheme } from "../../theme";
 
-export const styles: SxStyles = {};
+export const styles: SxStyles = {
+  root: {
+    width: "100%",
+  },
+  userMessage: {
+    p: 6,
+    display: "flex",
+    columnGap: 4,
+    bgcolor: "custom.white",
+    alignItems: "center",
+  },
+  authorIcon: {
+    width: 44,
+    height: 44,
+    alignSelf: "start",
+  },
+};
