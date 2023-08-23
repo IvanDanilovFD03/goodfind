c97a2b040890346010ed2f2dac735b8852dbfda2
IvanDanilovFD03 <ivd@empat.tech>
2023-08-23 11:18:34 +0300

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { FC, useState } from "react";
+import { useBreakpoints } from "../../hooks/useBreakpoints";
+import { Box } from "../ui/Box";
+import { Typography } from "../ui/Typography";
+
+import { styles } from "./styles";
+import { IconButton } from "../ui/IconButton";
+import { CancelIcon } from "../icons/CancelIcon";
+
+export interface MessageWidgetProps {
+  title: string;
+  setMessageWidgetOpen: (value: React.SetStateAction<boolean>) => void;
+}
+
+export const Header: FC<MessageWidgetProps> = ({
+  title,
+  setMessageWidgetOpen,
+}) => {
+  return (
+    <Box sx={styles.root}>
+      <Typography variant="h3" color="custom.white">
+        {title}
+      </Typography>
+      <IconButton onClick={() => setMessageWidgetOpen(false)}>
+        <CancelIcon size="md" />
+      </IconButton>
+    </Box>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { SxStyles } from "../../types/styles";
+import { basicTheme } from "../../theme";
+
+export const styles: SxStyles = {
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+    px: 5,
+    py: "10px",
+    bgcolor: "custom.black",
+    alignItems: "center",
+  },
+};
