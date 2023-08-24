e8b127ae7f9c0cf3c037c4b8e0e2f78fa43b1949
IvanDanilovFD03 <ivd@empat.tech>
2023-08-24 13:17:30 +0300

    feat: update MessageWidget with new props

--- src/components/MessageWidget/index.tsx
+++ src/components/MessageWidget/index.tsx
@@ -4,14 +4,13 @@ import { Box } from "../ui/Box";
 import { styles } from "./styles";
 import { Header } from "../Header";
 import { MessageList } from "../MessageList";
-import { UserMessage } from "../../types/api";
-import { AIMessage } from "../../types/api";
+import { Message } from "../../types/api";
 
 export interface MessageWidgetProps {
   widgetTitle: string;
   messageWidgetOpen: boolean;
   setMessageWidgetOpen: (value: React.SetStateAction<boolean>) => void;
-  messages: Array<UserMessage | AIMessage>;
+  messages: Message[];
 }
 
 export const MessageWidget: FC<MessageWidgetProps> = ({
