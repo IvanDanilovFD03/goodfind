f40bb6ecc86668e91acdee1cb0033500db528536
IvanDanilovFD03 <ivd@empat.tech>
2023-08-24 13:16:11 +0300

    feat: update Message component with new props and MessageCard component

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -4,59 +4,40 @@ import { Avatar } from "../ui/Avatar";
 import { Box } from "../ui/Box";
 import { styles } from "./styles";
 import { Typography } from "../ui/Typography";
-import userImage from "../app/userImage/userImage.png";
 import { MessageIcon } from "../icons/MessageIcon";
 import { SxProps } from "@mui/material";
-import { AIMessage, UserMessage } from "../../types/api";
+import { Message as MessageApi } from "../../types/api";
+import { MessageCard } from "../MessageCard";
 
 export interface MessageProps {
-  content: UserMessage | AIMessage;
+  messageContent: MessageApi;
 }
 
-export const Message: FC<MessageProps> = ({ content }) => {
-  const messageStyle = (author: "user" | "ai"): SxProps =>
-    author === "user"
-      ? ({
-          ...styles.message,
-          ...styles.userMessage,
-        } as SxProps)
-      : ({
-          ...styles.message,
-          ...styles.aiMessage,
-        } as SxProps);
-
-  const iconStyle = (author: "user" | "ai"): SxProps =>
-    author === "user"
-      ? ({
-          ...styles.authorIcon,
-        } as SxProps)
-      : ({
-          ...styles.authorIcon,
-          ...styles.aiIcon,
-        } as SxProps);
+export const Message: FC<MessageProps> = ({ messageContent }) => {
+  const { role, content, products } = messageContent;
 
   return (
     <Box sx={styles.root}>
-      {content.author === "user" ? (
-        <Box sx={messageStyle(content.author)}>
-          <Avatar
-            alt="user image"
-            variant="square"
-            src={userImage}
-            sx={iconStyle(content.author)}
-          />
-          <Typography variant="textMessage" sx={styles.userText}>
-            {content.text}
+      {role === 1 ? (
+        <Box sx={styles.userMessage}>
+          <Typography
+            variant="textMessage"
+            color="custom.white"
+            sx={styles.userText}
+          >
+            {content}
           </Typography>
         </Box>
       ) : (
-        <Box sx={messageStyle(content.author)}>
-          <Avatar alt="ai image" variant="square" sx={iconStyle(content.author)}>
-            <MessageIcon size="md" />
-          </Avatar>
-          <Typography variant="textMessage" sx={styles.userText}>
-            {content.text}
-          </Typography>
+        <Box sx={styles.aiMessageContainer}>
+          <Box sx={styles.aiMessage}>
+            <Avatar alt="ai image" variant="square" sx={styles.aiIcon}>
+              <MessageIcon size="md" />
+            </Avatar>
+            <Box sx={styles.aiMessageText}>
+              <MessageCard text={content} products={products} />
+            </Box>
+          </Box>
         </Box>
       )}
     </Box>
--- src/components/Message/styles.ts
+++ src/components/Message/styles.ts
@@ -5,24 +5,47 @@ export const styles: SxStyles = {
   root: {
     width: "100%",
   },
-  message: {
-    p: 6,
+  userMessage: {
+    py: 3,
+    px: 6,
+    display: "flex",
+  },
+  userText: {
+    bgcolor: "custom.black",
+    width: 300,
+    py: 4,
+    px: 6,
+    ml: "auto",
+  },
+  aiMessageContainer: {
+    py: 3,
+    px: 6,
+  },
+  aiMessage: {
     display: "flex",
     columnGap: 4,
     alignItems: "center",
   },
-  userMessage: {
+  aiMessageText: {
     bgcolor: "custom.white",
+    width: "100%",
+    p: 4,
   },
-  aiMessage: {
-    bgcolor: "custom.grayLight",
-  },
-  authorIcon: {
+  aiIcon: {
     width: 44,
     height: 44,
     alignSelf: "start",
-  },
-  aiIcon: {
     bgcolor: "custom.green",
+  },
+  productList: {
+    p: 0,
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 2,
+    width: "100%",
+  },
+  productListItem: {
+    p: 0,
+    width: "100%",
   },
 };
