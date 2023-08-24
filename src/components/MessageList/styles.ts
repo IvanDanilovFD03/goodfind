9a22e7ee7138bacd6d36e425b46ad9d50afc387f
IvanDanilovFD03 <ivd@empat.tech>
2023-08-24 13:23:29 +0300

    feat: add custom scrollbar

--- src/components/MessageList/styles.ts
+++ src/components/MessageList/styles.ts
@@ -7,6 +7,18 @@ export const styles: SxStyles = {
     p: 0,
     height: "calc(100vh - 44px - 100px)",
     overflowY: "scroll",
+    "::-webkit-scrollbar": {
+      width: 6,
+    },
+    "::-webkit-scrollbar-track": {
+      bgcolor: "custom.white",
+    },
+    "::-webkit-scrollbar-thumb": {
+      bgcolor: "#D0D0D0",
+    },
+    "::-webkit-scrollbar-thumb:hover": {
+      background: "#555",
+    },
     [basicTheme.breakpoints.up("tablet")]: {
       height: "calc(70vh - 44px)",
     },
