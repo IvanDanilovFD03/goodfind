09294acc90f885f6180e585c76fa02580f8e5602
IvanDanilovFD03 <ivd@empat.tech>
2023-08-24 13:17:09 +0300

    feat: update MessageList with new props

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -6,21 +6,25 @@ import { ListItem } from "../ui/ListItem";
 
 import { styles } from "./styles";
 import { Message } from "../Message";
-import { UserMessage } from "../../types/api";
-import { AIMessage } from "../../types/api";
+import { Message as MessageProps } from "../../types/api";
 
 export interface MessageListProps {
-  messages: Array<UserMessage | AIMessage>;
+  messages: MessageProps[];
 }
 
 export const MessageList: FC<MessageListProps> = ({ messages }) => {
   return (
     <List sx={styles.root}>
-      {messages.map((message) => (
-        <ListItem sx={styles.listItem}>
-          <Message content={message} />
-        </ListItem>
-      ))}
+      {messages
+        .map((message) => (
+          <ListItem
+            sx={styles.listItem}
+            key={`${message.role + message.content + Math.random()}`}
+          >
+            <Message messageContent={message} />
+          </ListItem>
+        ))
+        .reverse()}
     </List>
   );
 };
--- src/components/MessageList/styles.ts
+++ src/components/MessageList/styles.ts
@@ -5,6 +5,11 @@ export const styles: SxStyles = {
   root: {
     width: "100%",
     p: 0,
+    height: "calc(100vh - 44px - 100px)",
+    overflowY: "scroll",
+    [basicTheme.breakpoints.up("tablet")]: {
+      height: "calc(70vh - 44px)",
+    },
   },
   listItem: {
     p: 0,
