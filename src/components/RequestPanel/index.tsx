f247635a99aa56687157c1395eaa759b2db82ce3
IvanDanilovFD03 <ivd@empat.tech>
2023-08-25 13:35:23 +0300

    feat: update RequestPanel with main functionality

--- src/components/RequestPanel/index.tsx
+++ src/components/RequestPanel/index.tsx
@@ -1,17 +1,98 @@
-import { FC, useState } from "react";
-import { useBreakpoints } from "../../hooks/useBreakpoints";
+import { FC, useCallback, useEffect, useState } from "react";
 import { Box } from "../ui/Box";
+import { Input } from "../ui/Input";
+import EmojiPicker from "emoji-picker-react";
+import { EmojiClickData } from "emoji-picker-react";
+
+import { IconButton } from "../ui/IconButton";
+import { SxProps } from "@mui/material";
+
 import { styles } from "./styles";
-import { Typography } from "@mui/material";
+import { SendIcon } from "../icons/SendIcon";
+import { EmojiIcon } from "../icons/EmojiIcon";
 
-export interface RequestPanelProps {}
+export interface RequestPanelProps {
+  setEnteredTextMessage: (value: React.SetStateAction<string>) => void;
+  activeSendRequest: boolean;
+  setActiveSendRequest: (value: React.SetStateAction<boolean>) => void;
+}
 
-export const RequestPanel: FC<RequestPanelProps> = ({}) => {
+export const RequestPanel: FC<RequestPanelProps> = ({
+  setEnteredTextMessage,
+  activeSendRequest,
+  setActiveSendRequest,
+}) => {
+  const [emojiPickerActive, setEmojiPickerActive] = useState(false);
+  const [textMessage, setTextMessage] = useState("");
+
+  const emojiPickHandler = useCallback(
+    (emojiData: EmojiClickData) => {
+      setTextMessage(textMessage + emojiData.emoji);
+    },
+    [textMessage]
+  );
+
+  const textEnterHandler = (
+    event: React.KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      setEnteredTextMessage(textMessage);
+      setActiveSendRequest(true);
+      setTextMessage("")
+    }
+  };
+
+  const activeButtonStyles = (activeSendRequest: boolean): SxProps =>
+    !activeSendRequest
+      ? styles.sendButton
+      : ({ ...styles.sendButton, ...styles.disabledSendButton } as SxProps);
+
+  useEffect(() => {
+    if (textMessage === "") {
+      setEnteredTextMessage(textMessage);
+    }
+  }, [setEnteredTextMessage, textMessage]);
+
   return (
     <Box sx={styles.root}>
-      <Typography variant="h1" color="custom.white">
-        REQUEST PANEL
-      </Typography>
+      <Box sx={styles.input}>
+        <Input
+          sx={styles.inputBase}
+          placeholder="Write a reply..."
+          onChange={(event) => setTextMessage(event.target.value)}
+          value={textMessage}
+          onKeyDown={(event) => textEnterHandler(event)}
+          disabled={activeSendRequest}
+        />
+        <IconButton
+          sx={styles.emojiButton}
+          onClick={() => setEmojiPickerActive(!emojiPickerActive)}
+        >
+          <EmojiIcon size="md" />
+        </IconButton>
+        {emojiPickerActive && (
+          <Box sx={styles.emojiPicker}>
+            <EmojiPicker
+              width={300}
+              height={350}
+              onEmojiClick={(emojiData: EmojiClickData) =>
+                emojiPickHandler(emojiData)
+              }
+            />
+          </Box>
+        )}
+      </Box>
+      <IconButton
+        sx={activeButtonStyles(activeSendRequest)}
+        onClick={() => {
+          setEnteredTextMessage(textMessage);
+          setActiveSendRequest(true);
+          setTextMessage("")
+        }}
+        disabled={activeSendRequest}
+      >
+        <SendIcon size="md" sx={styles.sendButtonIcon} />
+      </IconButton>
     </Box>
   );
 };
--- src/components/RequestPanel/styles.ts
+++ src/components/RequestPanel/styles.ts
@@ -12,10 +12,54 @@ export const styles: SxStyles = {
     alignItems: "center",
     justifyContent: "center",
     boxShadow: "20px 0px 16px 0px rgba(0, 0, 0, 0.25)",
+    p: "10px",
+    columnGap: 2,
     [basicTheme.breakpoints.up("tablet")]: {
       width: 540,
       bottom: 100,
       right: 40,
     },
+  },
+  sendButton: {
+    bgcolor: "custom.green",
+    p: "8.533px",
+    borderRadius: 0,
+    "&:hover": {
+      bgcolor: "custom.gray",
+    },
+  },
+  disabledSendButton: {
+    "&.Mui-disabled": {
+      bgcolor: "custom.gray",
+    },
+  },
+  input: {
+    border: "1px solid #DADADA",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  inputBase: {
+    width: "90%",
+    color: "custom.white",
+    input: {
+      px: 4,
+      py: 2,
+    },
+    "&:after": {
+      borderBottom: "none",
+    },
+  },
+  sendButtonIcon: {
+    fill: "none",
+  },
+  emojiButton: {
+    ml: "auto",
+    mr: 4,
+  },
+  emojiPicker: {
+    position: "absolute",
+    top: -350,
+    right: 60,
   },
 };
