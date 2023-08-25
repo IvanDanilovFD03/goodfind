13f5eb21efddac328d63d807652c563c7fc48b47
IvanDanilovFD03 <ivd@empat.tech>
2023-08-25 13:36:00 +0300

    feat: update RequestPanel's props in MainPageView

--- src/pages/MainPageView/MainPageView.tsx
+++ src/pages/MainPageView/MainPageView.tsx
@@ -2,16 +2,23 @@ import { FC, useState } from "react";
 
 import { styles } from "./styles";
 import { Box } from "../../components/ui/Box";
-import { Typography } from "../../components/ui/Typography";
 import { MessageWidget } from "../../components/MessageWidget";
 import { RequestPanel } from "../../components/RequestPanel";
 import { Message } from "../../types/api";
 
 interface MainPageViewProps {
   messages: Message[];
+  setEnteredTextMessage: (value: React.SetStateAction<string>) => void;
+  activeSendRequest: boolean;
+  setActiveSendRequest: (value: React.SetStateAction<boolean>) => void;
 }
 
-export const MainPageView: FC<MainPageViewProps> = ({ messages }) => {
+export const MainPageView: FC<MainPageViewProps> = ({
+  messages,
+  setEnteredTextMessage,
+  activeSendRequest,
+  setActiveSendRequest,
+}) => {
   const [messageWidgetOpen, setMessageWidgetOpen] = useState(true); //change it on false when everything will be ready
   return (
     <Box sx={styles.root}>
@@ -23,7 +30,11 @@ export const MainPageView: FC<MainPageViewProps> = ({ messages }) => {
           messages={messages}
         />
       )}
-      <RequestPanel />
+      <RequestPanel
+        setEnteredTextMessage={setEnteredTextMessage}
+        activeSendRequest={activeSendRequest}
+        setActiveSendRequest={setActiveSendRequest}
+      />
     </Box>
   );
 };
