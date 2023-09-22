a12adcdd4424c043e7ff86d00f60bc06223a38eb
IvanDanilovFD03 <ivd@empat.tech>
2023-09-22 14:12:23 +0300

    feat: add Product type

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,14 +1,16 @@
 export type Message = {
   role: number;
   content: string;
-  products?: {
-    id: number;
-    title: string;
-    image: string | null;
-    short_description: string;
-    meta: {
-      price: string;
-      product_url: string;
-    }
-  }[];
+  products?: Product[] | "loadingProducts";
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  image: string | null;
+  short_description: string;
+  meta: {
+    price: string;
+    product_url: string;
+  };
 };
